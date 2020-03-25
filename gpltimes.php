f35dc18cdac3e39f0419d4ffd46ba94595388053
abhijeetmandal2502 <42529709+abhijeetmandal2502@users.noreply.github.com>
2020-03-25 19:36:13 +0530

    version update

--- gpltimes.php
+++ gpltimes.php
@@ -6,7 +6,7 @@
 Plugin Name: GPL Times
 Plugin URI: https://gpltimes.com/
 Description: GPLTimes Auto Updater 
-Version: 1.0.2
+Version: 1.0.3
 Author: Abhijeet Mandal
 Author URI: https://www.thenwguru.com/
 License: GPLv2 or later
