2443a5ea58af70f4a6cad5ccdf8389c37e8e3b5a
abhijeetmandal2502 <42529709+abhijeetmandal2502@users.noreply.github.com>
2020-03-26 11:08:27 +0530

    update version
    time issue when user comes after long time

--- gpltimes.php
+++ gpltimes.php
@@ -6,7 +6,7 @@
 Plugin Name: GPL Times
 Plugin URI: https://gpltimes.com/
 Description: GPLTimes Auto Updater 
-Version: 1.0.4
+Version: 1.0.5
 Author: Abhijeet Mandal
 Author URI: https://www.thenwguru.com/
 License: GPLv2 or later
