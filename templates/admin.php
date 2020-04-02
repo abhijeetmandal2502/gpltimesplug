164071d1e4ab815c2c7d59790b53e0455a4cac3d
asoutiyal <52098989+asoutiyal@users.noreply.github.com>
2020-04-02 06:39:41 +0530

    Update admin.php

--- templates/admin.php
+++ templates/admin.php
@@ -2,14 +2,8 @@
 <div class="wrap">
 	<?php settings_errors(); ?>
 
-	<!-- <ul class="nav nav-tabs">
-		<li class="active"><a href="#tab-1">Activate  Settings</a></li>
-		
-		
-	</ul> -->
-
-	<div class="tab-content">
-		<div id="tab-1" class="tab-pane active">
+	
+		<div class="tab-pane">
 
 			<form method="post" action="options.php">
 				<?php 
@@ -27,5 +21,4 @@
 
 
 		
-	</div>
 </div>
